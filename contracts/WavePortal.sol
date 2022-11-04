565fe9ce5453d971c8cd11b2c2ca631a121014d3
Mohsin Ali <59974279+imohsinali@users.noreply.github.com>
2022-11-04 11:57:00 +0500

    Create WavePortal.sol

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor()
+    {
+                console.log("Yo yo, I am a contract and I am smart");
+
+    }
+    
+}
